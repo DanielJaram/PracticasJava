4de320198d8dfa996f5b2cb7f2cbe6b5e741156a
JARAMILLO <JARAMILLO@DESKTOP-DHSAPM8>
2024-04-07 12:20:12 -0500

    Avanzando

--- CursoJava/src/firstjavaclass/Exercises.java
+++ CursoJava/src/firstjavaclass/Exercises.java
@@ -57,4 +57,30 @@ public class Exercises {
 		}
 		return false;
 	}
+
+	/*
+	 * Coding Exercise: Is Valid Triangle You are given three integer inputs that
+	 * represent the angles of a triangle. You need to complete a method named
+	 * isValidTriangle(int angle1, int angle2, int angle3), inside a class named
+	 * TriangleValidator. This method should return a boolean value – true if the
+	 * given angles form a valid triangle, and false if they do not.
+	 * 
+	 * The conditions for a valid triangle are:
+	 * 
+	 * Each of the angles must be a positive integer.
+	 * 
+	 * The sum of all the three angles must be exactly 180.
+	 */
+	
+	public class TriangleValidator {
+	    public boolean isValidTriangle(int angle1, int angle2, int angle3) {
+	        // write your code here
+	        int sum = angle1 + angle2 + angle3;
+	        if(angle1 > 0 && angle2 > 0 && angle3 > 0 && sum == 180){
+	            return true;
+	        }
+	        return false;
+	    }
+	}
+	
 }
