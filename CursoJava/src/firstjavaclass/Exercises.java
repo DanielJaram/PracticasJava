64df0f8ea3368deece8f8666c4a494cf0217ae88
Daniel Jaramillo <danieljaramillomerino@gmail.com>
2024-04-06 20:33:30 -0500

    Correccion

--- CursoJava/src/firstjavaclass/Exercises.java
+++ CursoJava/src/firstjavaclass/Exercises.java
@@ -50,7 +50,6 @@ public class Exercises {
 	 */
 
 	public boolean isPass(int marks) {
-
 		// Update Code : If the student's marks are greater than 50, they have passed
 		// the exam.
 		if (marks > 50) {
