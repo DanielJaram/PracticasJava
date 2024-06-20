920ff58944b3ff850a3e478088a451d1215ad18d
PC <PC@PC>
2024-06-19 22:37:32 -0500

    ConditionStatement

---
+++ Conditionals/src/ExercisesC/IfStatementRunner.java
@@ -0,0 +1,18 @@
+package ExercisesC;
+
+public class IfStatementRunner {
+
+	public static void main(String[] args) {
+		int i = 25;
+		if (i == 25) {
+			System.out.println("i = 25");
+		}else if (i == 24) {
+			System.out.println("i = 24");
+		}else if (i == 23) {
+			System.out.println("i = 23");
+		}
+		else {
+			System.out.println(" i != 23 and i != 24 and i != 25");
+		}
+	}
+}
