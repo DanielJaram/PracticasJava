5f7afbb48ca706de55237e8fdea871ec43ee07f9
PC <PC@PC>
2024-06-20 20:18:07 -0500

    avance

---
+++ Conditionals/src/ExercisesC/MenuRunner.java
@@ -0,0 +1,14 @@
+package ExercisesC;
+
+import java.util.Scanner;
+
+public class MenuRunner {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter Number1: ");
+		int number1 = scanner.nextInt();
+		System.out.println("The number your entered is - " + number1);
+	}
+
+}
