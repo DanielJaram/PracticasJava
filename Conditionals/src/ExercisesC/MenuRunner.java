12a0d3d4697028b4bd4670b05d16641da3e84c1f
PC <PC@PC>
2024-06-22 12:44:55 -0500

    Avanzando

--- Conditionals/src/ExercisesC/MenuRunner.java
+++ Conditionals/src/ExercisesC/MenuRunner.java
@@ -16,6 +16,12 @@ public class MenuRunner {
 							+ "\n 2 - Substract" + "\n 3 - Divide" + "\n 4 - Multiply" );
 		int operation = scanner.nextInt();
 		int result = 0;
+		//result = performOperationUsingNestedIfElse(number1, number2, operation, result);
+		//System.out.println("El resultado es " + result);
+		performOperationUsingNestedSwitch(number1, number2, operation);
+	}
+
+	private static int performOperationUsingNestedIfElse(int number1, int number2, int operation, int result) {
 		if (operation == 1) {
 			result = number1 + number2;
 		}else if (operation == 2) {
@@ -25,7 +31,32 @@ public class MenuRunner {
 		}else if (operation == 4) {
 			result = number1 * number2;
 		}
-		System.out.println("El resultado es " + result);
+		else {
+			System.out.println("Invalid Operation");
+		}
+		return result;
+	}
+	
+	private static void performOperationUsingNestedSwitch(int number1, int number2, int operation) {
+		switch(operation) {
+		case 1:
+			System.out.println("Result " + (number1 + number2)); 
+			break;
+		case 2:
+			System.out.println("Result " + (number1 - number2));
+			break;
+		case 3: 
+			System.out.println("Result " + (number1 / number2));
+			break;
+		case 4:
+			System.out.println("Result " + (number1 * number2));
+			break;
+		default:
+			System.out.println("Invalid Operation");
+			break;
+		}
+
+		
 	}
 
 }
